cc7f8d9b49c459042fce95baf362cc05be0210d8
m.jesus <138052716+MuriloDeJesus@users.noreply.github.com>
2023-12-22 00:06:40 -0300

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Atos Ignite Program!"
+    return "Hello Atos Ignite Program 2023!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
